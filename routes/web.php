dd06d39c0e783b553197325cbb2f5f68059daf9a
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-12-14 14:40:30 +0100

    passaggio dinamico data array

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    $data = [
+
+        "name" => "Matteo",
+        "lastName" => "Giovagnotti",
+        "booleanClass" => 73,
+        "lastArgument" => "Laravel" 
+
+    ];
+
+
+
+    return view('home', $data);
 });
